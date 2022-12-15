c663b0ea3a83d5d48be7f4aad888e0d8487a77e5
Rekhtina Lyudmila <Rehtina520@mail.ru>
2022-12-15 21:24:42 +0700

    Add Program6_1

---
+++ Homework6/Program6_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите числа через пробел: ");
+string str = Console.ReadLine();
+
+var arrStr = str.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+int[] arr = Array.ConvertAll(arrStr, Convert.ToInt32);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Количество элементов больше нуля: {count}");
